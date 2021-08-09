169a2239d547fc21c1641aeb0dd52cb1f4bfd5e4
apolkingg8 <apolkingg8@gmail.com>
2021-08-09 15:36:40 +0800

    :sparkles: answer3

--- answer3.test.ts
+++ answer3.test.ts
@@ -2,15 +2,32 @@ import foo from "./answer3";
 
 describe("answer3", ()=> {
 
+    function recur(n, cur) {
+
+        if (!cur) {
+            cur = 0;
+        }
+
+        if (n < 2) {
+            throw new Error('Invalid input');
+        }
+
+        if (n === 2) {
+            return 1 / n + cur;
+        }
+
+        return recur(n - 1, cur + 1 / (n * (n -1)));
+    }
+
     test("foo()", ()=> {
-        expect(foo(10, 10)).toEqual(10.9)
-        expect(foo(10, 11)).toEqual(11.9)
-        expect(foo(10, 12)).toEqual(12.9)
-        expect(foo(10, 13)).toEqual(13.9)
-        expect(foo(5, 10)).toEqual(10.8)
-        expect(foo(5, 11)).toEqual(11.8)
-        expect(foo(5, 12)).toEqual(12.8)
-        expect(foo(5, 13)).toEqual(13.8)
+        expect(foo(7, 2)).toEqual(recur(7, 2))
+        expect(foo(3, 4)).toEqual(recur(3, 4))
+        expect(foo(10, 10)).toEqual(recur(10, 10))
+        expect(foo(20, 11)).toEqual(recur(20, 11))
+        expect(foo(5, 7)).toEqual(recur(5, 7))
     })
 
+    test("foo() bad input", ()=> {
+        expect(()=> {foo(1, 2)}).toThrow('Invalid input')
+    })
 })
--- answer3.ts
+++ answer3.ts
@@ -1,5 +1,10 @@
 
 const foo = (n: number, cur: number)=> {
+
+    if(n < 2) {
+        throw `Invalid input`
+    }
+
     cur = cur || 0
 
     for(;n>2;n-=1) {
